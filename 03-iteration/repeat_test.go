a5d0d4e71ffb533e23841ff55a8ba21eaf647c80
UserAlen <93518707+UserAlen@users.noreply.github.com>
2023-12-30 16:15:36 +0300

    added benchmark and example to test

--- 03-iteration/repeat_test.go
+++ 03-iteration/repeat_test.go
@@ -1,12 +1,30 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
+	repeated := Repeat("a", 5)
 	expected := "aaaaa"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
+}
+
+// to run the benchmarks do "go test -bench=." or
+// on Windows Powershell "go test -bench="."
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+// to test Example do "go test -v"
+func ExampleRepeat() {
+	result := Repeat("a", 5)
+	fmt.Println(result)
+	// Output: aaaaa
 }
