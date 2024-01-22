a512899e6c3e33e680df2e2134414401135fb536
UserAlen <93518707+UserAlen@users.noreply.github.com>
2024-01-22 21:54:00 +0300

    something added to replace.go

--- 03-iteration/strings_contains_toupper/replace.go
+++ 03-iteration/strings_contains_toupper/replace.go
@@ -8,10 +8,25 @@ func Replace(sentence, sentenceWord, wordToReplace string) string {
 	if strings.Contains(sentenceLower, sentenceWordLower) {
 		// TODO: Change with split or arrays
 		// - Sentence needs to be the same except sentenceWord -> wordToReplace
+
+		var sentenceWords []string = SentenceToWords(sentence)
+		for i, word := range sentenceWords {
+			if strings.ToLower(word) == strings.ToLower(sentenceWord) {
+				//idk
+			}
+		}
+
 		result := strings.Replace(sentenceLower, sentenceWordLower, wordToReplace, 1)
 		return result
 	}
 	return getErrorMessage(sentence, sentenceWordLower, wordToReplace)
+}
+
+func SentenceToWords(sentence string) []string {
+	// instead of "spacebar" can be any separator - a collection of punctuatian marks
+	// or way easier - using the alphabet of the language with slices instead of strings package
+
+	return strings.Split(sentence, " ")
 }
 
 func getErrorMessage(sentence, sentenceWord, wordToReplace string) string {
