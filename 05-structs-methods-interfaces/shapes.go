d4027d0664aa5b6fd45908d38bb20809a6724de2
UserAlen <93518707+UserAlen@users.noreply.github.com>
2024-02-10 17:31:49 +0300

    methods and interfaces

--- 05-structs-methods-interfaces/shapes.go
+++ 05-structs-methods-interfaces/shapes.go
@@ -1,9 +1,42 @@
 package main
 
-func Perimeter(rectangle Rectangle) float64 {
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Height float64
+	Width  float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return (r.Height + r.Width) * 2
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * math.Pow(c.Radius, 2)
+}
+
+/* func Perimeter(rectangle Rectangle) float64 {
 	return (rectangle.Width + rectangle.Height) * 2
 }
 
 func Area(rectangle Rectangle) float64 {
 	return rectangle.Width * rectangle.Height
-}
+} */
+
+// You  can't do that in Go
+
+/* func Area(circle Circle) float64 {
+	return circle.Radius * math.Pow(math.Pi, 2)
+} */
--- 05-structs-methods-interfaces/shapes_test.go
+++ 05-structs-methods-interfaces/shapes_test.go
@@ -2,14 +2,12 @@ package main
 
 import "testing"
 
-type Rectangle struct {
-	Height float64
-	Width  float64
-}
-
 func TestPerimeter(t *testing.T) {
+
+	checkPerimeter := func(t *testing.TB, shape Shape)
+
 	rectangle := Rectangle{10.0, 10.0}
-	got := Perimeter(rectangle)
+	got := rectangle.Perimeter()
 	want := 40.0
 
 	if got != want {
@@ -18,20 +16,26 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
+
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	}
+
 	t.Run("rectangles", func(t *testing.T) {
 		rectangle := Rectangle{Height: 10.0, Width: 20.0}
-		got := Area(rectangle)
 		want := 200.0
 
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+		checkArea(t, rectangle, want)
 	})
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{10}
-		got := Area(circle)
 		want := 314.1592653589793
 
+		checkArea(t, circle, want)
 	})
 
 }
