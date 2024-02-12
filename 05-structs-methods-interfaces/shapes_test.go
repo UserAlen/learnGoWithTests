665795afef353c2ac3d17ff1baa2c64e4f89cc27
UserAlen <93518707+UserAlen@users.noreply.github.com>
2024-02-12 18:36:47 +0300

    table driven tests

--- 05-structs-methods-interfaces/shapes_test.go
+++ 05-structs-methods-interfaces/shapes_test.go
@@ -4,8 +4,6 @@ import "testing"
 
 func TestPerimeter(t *testing.T) {
 
-	checkPerimeter := func(t *testing.TB, shape Shape)
-
 	rectangle := Rectangle{10.0, 10.0}
 	got := rectangle.Perimeter()
 	want := 40.0
@@ -17,25 +15,46 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
+	//table driven test
+
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 10.0, Height: 20.0}, hasArea: 200.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.hasArea {
+			// The %#v format string will print out our struct with the values in its field
+			// as in Circle{Radius: 10}
+			t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
 		}
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{Height: 10.0, Width: 20.0}
-		want := 200.0
+	/* 	checkArea := func(t testing.TB, shape Shape, want float64) {
+	   		t.Helper()
+	   		got := shape.Area()
+	   		if got != want {
+	   			t.Errorf("got %g want %g", got, want)
+	   		}
+	   	}
 
-		checkArea(t, rectangle, want)
-	})
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		want := 314.1592653589793
+	   	t.Run("rectangles", func(t *testing.T) {
+	   		rectangle := Rectangle{Height: 10.0, Width: 20.0}
+	   		want := 200.0
 
-		checkArea(t, circle, want)
-	})
+	   		checkArea(t, rectangle, want)
+	   	})
+	   	t.Run("circles", func(t *testing.T) {
+	   		circle := Circle{10}
+	   		want := 314.1592653589793
+
+	   		checkArea(t, circle, want)
+	   	}) */
 
 }
