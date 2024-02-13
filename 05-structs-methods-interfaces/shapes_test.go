64427a43ff2d05d9da08b2dd6ac518fd6628169c
UserAlen <93518707+UserAlen@users.noreply.github.com>
2024-02-13 16:42:47 +0300

    area done

--- 05-structs-methods-interfaces/shapes_test.go
+++ 05-structs-methods-interfaces/shapes_test.go
@@ -22,18 +22,21 @@ func TestArea(t *testing.T) {
 		shape   Shape
 		hasArea float64
 	}{
+		//for specific test "go test -run TestArea/Rectangle"
 		{name: "Rectangle", shape: Rectangle{Width: 10.0, Height: 20.0}, hasArea: 200.0},
 		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
 		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.hasArea {
-			// The %#v format string will print out our struct with the values in its field
-			// as in Circle{Radius: 10}
-			t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				// The %#v format string will print out our struct with the values in its field
+				// as in Circle{Radius: 10}
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
 	}
 
 	/* 	checkArea := func(t testing.TB, shape Shape, want float64) {
