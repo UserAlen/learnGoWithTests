27b5b5448dbc1d316a56f079fb924ece9bf1b755
UserAlen <93518707+UserAlen@users.noreply.github.com>
2024-02-14 16:59:14 +0300

    pointers + stringer

--- 06-pointers-errors/wallet_test.go
+++ 06-pointers-errors/wallet_test.go
@@ -1,16 +1,19 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
 
-	wallet.Deposit(10)
+	wallet.Deposit(Bitcoin(10))
 
-	got := wallet.Balance()
-	want := 10
+	got := wallet.Balance().String()
+	/* fmt.Printf("address of balance in test is %p \n", &wallet.balance) */
+	want := Bitcoin(10).String()
 
 	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		t.Errorf("got %s want %s", got, want)
 	}
 }
